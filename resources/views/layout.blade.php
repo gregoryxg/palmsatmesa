384c07bba77ce38b39e0bc0a3a1c9bbd6f4636b1
greg <gregory_gonzalez@sbcglobal.net>
2019-04-19 14:56:38 -0700

    Initial Commit

---
+++ resources/views/layout.blade.php
@@ -0,0 +1,13 @@
+<!DOCTYPE html>
+<html>
+<head>
+    <title></title>
+</head>
+<body>
+@yield('content')
+
+<a href="/">Home</a>
+<a href="/contact">Contact us</a>
+<a href="/about">About</a>
+</body>
+</html>
