ac5af26645bd89fa923392bf2d9c6b00db27b38a
greg <gregory_gonzalez@sbcglobal.net>
2019-05-07 18:30:46 -0700

    Initial Commit

--- resources/views/pages/carousel.blade.php
+++ resources/views/pages/carousel.blade.php
@@ -1,21 +1,63 @@
 <div id="palmsCarousel" class="carousel slide" data-ride="carousel">
     <div class="carousel-inner">
-        <div class="carousel-item active bg-secondary">
-{{--
-            <img src="{{ asset("img/palms_at_mesa_night.jpg") }}" alt="First slide"/>
---}}
-            <svg xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" width="100%" height="100%">
+        <div class="carousel-item active">
+            <svg class="bd-placeholder-img" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" width="100%" height="100%">
+                <image xlink:href="{{ asset("img/palms_at_mesa_entrance.jpg") }}" width="100%" height="100%"/>
+            </svg>
+        </div>
+        <div class="carousel-item">
+            <svg class="bd-placeholder-img" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" width="100%" height="100%">
                 <image xlink:href="{{ asset("img/palms_at_mesa_night.jpg") }}" width="100%" height="100%"/>
             </svg>
         </div>
-        <div class="carousel-item bg-secondary">
+        <div class="carousel-item">
             <svg class="bd-placeholder-img" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" width="100%" height="100%">
                 <image xlink:href="{{ asset("img/palms_at_mesa_pool_a.jpg") }}" width="100%" height="100%"/>
             </svg>
         </div>
-        <div class="carousel-item bg-secondary">
+        <div class="carousel-item">
+            <svg class="bd-placeholder-img" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" width="100%" height="100%">
+                <image xlink:href="{{ asset("img/palms_at_mesa_pool_b.jpg") }}" width="100%" height="100%"/>
+            </svg>
+        </div>
+        <div class="carousel-item">
             <svg class="bd-placeholder-img" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" width="100%" height="100%">
                 <image xlink:href="{{ asset("img/palms_at_mesa_courtyard.jpg") }}" width="100%" height="100%"/>
+            </svg>
+        </div>
+        <div class="carousel-item">
+            <svg class="bd-placeholder-img" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" width="100%" height="100%">
+                <image xlink:href="{{ asset("img/palms_at_mesa_clubhouse.jpg") }}" width="100%" height="100%"/>
+            </svg>
+        </div>
+        <div class="carousel-item">
+            <svg class="bd-placeholder-img" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" width="100%" height="100%">
+                <image xlink:href="{{ asset("img/palms_at_mesa_kitchen.jpg") }}" width="100%" height="100%"/>
+            </svg>
+        </div>
+        <div class="carousel-item">
+            <svg class="bd-placeholder-img" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" width="100%" height="100%">
+                <image xlink:href="{{ asset("img/palms_at_mesa_pool_room.jpg") }}" width="100%" height="100%"/>
+            </svg>
+        </div>
+        <div class="carousel-item">
+            <svg class="bd-placeholder-img" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" width="100%" height="100%">
+                <image xlink:href="{{ asset("img/palms_at_mesa_theater.jpg") }}" width="100%" height="100%"/>
+            </svg>
+        </div>
+        <div class="carousel-item">
+            <svg class="bd-placeholder-img" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" width="100%" height="100%">
+                <image xlink:href="{{ asset("img/palms_at_mesa_gym.jpg") }}" width="100%" height="100%"/>
+            </svg>
+        </div>
+        <div class="carousel-item">
+            <svg class="bd-placeholder-img" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" width="100%" height="100%">
+                <image xlink:href="{{ asset("img/palms_at_mesa_playground.jpg") }}" width="100%" height="100%"/>
+            </svg>
+        </div>
+        <div class="carousel-item">
+            <svg class="bd-placeholder-img" xmlns="http://www.w3.org/2000/svg" preserveAspectRatio="none" width="100%" height="100%">
+                <image xlink:href="{{ asset("img/palms_at_mesa_house.jpg") }}" width="100%" height="100%"/>
             </svg>
         </div>
     </div>
