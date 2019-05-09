6a43d12041dcc1d76ce8a68b630178da4c780888
greg <gregory_gonzalez@sbcglobal.net>
2019-05-09 08:55:52 -0700

    Initial Commit

---
+++ resources/views/layouts/guest_navlinks.blade.php
@@ -0,0 +1,12 @@
+<li class="@yield('active_home')">
+    <a class="nav-link" href="/">Home <span class="sr-only">(current)</span></a>
+</li>
+<li class="@yield('active_new_residents')">
+    <a class="nav-link" href="/new_residents">New Residents</a>
+</li>
+<li class="@yield('active_privacy')">
+    <a class="nav-link" href="/privacy">Privacy</a>
+</li>
+<li class="@yield('active_terms')">
+    <a class="nav-link" href="/terms">Terms of Service</a>
+</li>
