8e7672ecc59a738668c0eb10062da29acaeff460
greg <gregory_gonzalez@sbcglobal.net>
2019-05-15 12:52:59 -0700

    Initial Commit

--- vendor/laravel/framework/src/Illuminate/Foundation/Auth/AuthenticatesUsers.php
+++ vendor/laravel/framework/src/Illuminate/Foundation/Auth/AuthenticatesUsers.php
@@ -91,7 +91,6 @@ trait AuthenticatesUsers
     protected function credentials(Request $request)
     {
         return $request->only($this->username(), 'password');
-        //return array_merge($request->only($this->username(), 'password'), ['active'=>1]);
     }
 
     /**
