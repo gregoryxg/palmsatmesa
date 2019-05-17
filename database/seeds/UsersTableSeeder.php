48bb849e6ceeb79be67525b7e301732da4dcbe60
greg <gregory_gonzalez@sbcglobal.net>
2019-05-17 15:31:04 -0700

    Initial Commit

--- database/seeds/UsersTableSeeder.php
+++ database/seeds/UsersTableSeeder.php
@@ -42,7 +42,7 @@ class UsersTableSeeder extends Seeder
                 'administrator'=>false,
                 'active'=>true,
                 'gate_code'=>6956,
-                'profile_picture'=>'img/headshot_uploads/2.png',
+                'profile_picture'=>'img/headshot_uploads/2.jpg',
                 'mobile_phone'=>'910-691-9895',
                 'email'=>'elisagonzalez8842@gmail.com',
                 'password'=>bcrypt('tella100'),
